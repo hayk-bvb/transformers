27705f9f59a98e98a22bb39a147c399854f48f9e
Hayk <haykik.nazaryan@gmail.com>
2025-05-01 00:22:34 -0400

    some tweaks before I ship code to google notebook

--- bert_fine_tune.py
+++ bert_fine_tune.py
@@ -5,6 +5,8 @@ from sklearn.model_selection import train_test_split
 from transformers import BertTokenizer, BertConfig
 from transformers import BertForSequenceClassification, get_linear_schedule_with_warmup
 from torch.optim import AdamW
+import keras
+
 
 from tqdm import tqdm, trange
 
@@ -41,4 +43,199 @@ print("Tokenuze the first sentence:")
 print(tokenized_texts[0])
 
 # ---- Processing the data ----
-# Set the maximum sequence length. 
+# Set the maximum sequence length. The longest sequence in our training set is 47,
+# but we'll leave room on the end anyway.
+# In the original paper, the authors used a length of 512.
+MAX_LEN = 128
+# Use the BERT tokenizer to convert the tokens to their index numbers in the BERT vocabulary
+input_ids = [tokenizer.convert_tokens_to_ids(x) for x in tokenized_texts]
+
+# Pad our input tokens
+input_ids = keras.utils.pad_sequences(input_ids, maxlen=MAX_LEN, dtype='long', truncating='post', padding='post')
+
+# ---- Create attention masks ----
+attention_masks = []
+# Create a mask of 1s for each token followed by 0s for padding
+for seq in input_ids:
+    seq_mask = [float(i>0) for i in seq]
+    attention_masks.append(seq_mask)
+
+
+# ---- Splitting data into train and validation sets ----
+# Use train_test_split to split our data into train and validation sets for training
+train_inputs, validation_inputs, train_labels, validation_labels = train_test_split(input_ids, labels, random_state=2018, test_size=0.1)
+train_masks, validation_masks, _, _ = train_test_split(attention_masks, input_ids, random_state=2018, test_size=0.1)
+
+
+# ---- Converting all the data into torch tensors ----
+# Torch tensors are the required datatype for our model
+train_inputs = torch.tensor(train_inputs)
+validation_inputs = torch.tensor(validation_inputs)
+train_labels = torch.tensor(train_labels)
+validation_labels = torch.tensor(validation_labels)
+train_masks = torch.tensor(train_masks)
+validation_masks = torch.tensor(validation_masks)
+
+
+# ---- Selecting a Batch Size and creating an Iterator ----
+# Select a batch size for training. For fine-tuninng BERT on a specific task, the authors recommend a batch size of 16 or 32.
+batch_size = 32
+# Create an iterator of our data with torch DataLoader. This helps save on memory during training because, unlike a for-loop,
+# with an iterator the entire dataset does not need to be loaded into memory
+train_data = TensorDataset(train_inputs, train_masks, train_labels)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
+validation_sampler = SequentialSampler(validation_data)
+validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size= batch_size)
+
+
+# ---- BERT Model configuration ----
+# Initializing a BERT bert-base-uncased style configuration
+# ---- Transformer Installation
+from transformers import BertModel, BertConfig
+configuration = BertConfig()
+# Initializing a model from the bert-base-uncased style configuration
+model = BertModel(configuration)
+# Accessing the model configuration
+configuration = model.config
+# print(configuration)
+
+# ---- Loading the Hugging Face Bert uncased base model
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
+model = nn.DataParallel(model)
+model.to(device)
+
+# ---- Optimizer Grouped Parameters ----
+# Don't apply weight decay to any parameter whose names include these tokens.
+# (Here, the BERT doesn't have 'gamma' or 'beta' parameters, only 'bias' terms)
+param_optimizer = list(model.named_parameters())
+no_decay = ['bias', 'LayerNorm.weight']
+# Separate the 'weight' parameters from the 'bias' parameters.
+# - For the 'weight' parameters, this specifies a 'weight_decay_rate' of 0.01.
+
+# - For the 'bias' parameters, this specifices a 'weight_decay_rate' is 0.0.
+optimizer_grouped_parameters = [
+    # Filter for all parameters which *don't* include 'bias', 'gamma', and 'beta'.
+    {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.1},
+
+     # Filter for parameters which *do* include those.
+     {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+      'weight_decay_rate': 0.0}
+]
+# Note - 'optimizer_grouped_parameters' only includes the parameter values, not the names.
+
+# ---- The Hyperparameters for the Training Loop ----
+epochs = 4
+
+optimizer = AdamW(optimizer_grouped_parameters,
+                  lr = 2e-5, # args.learning_rate - default is 5e-5, our notebook had 2e-5
+                  eps = 1e-8 # args.adam_epsilon  - default is 1e-8.
+                  )
+# Total number of training steps is number of batches * number of epochs.
+# `train_dataloader` contains batched data so `len(train_dataloader)` gives 
+# us the number of batches.
+total_steps = len(train_dataloader) * epochs
+
+# Create the learning rate scheduler.
+scheduler = get_linear_schedule_with_warmup(optimizer, 
+                                            num_warmup_steps = 0, # Default value in run_glue.py
+                                            num_training_steps = total_steps)
+
+
+# Creating the Accuracy Measurement Function
+# Function to calculate the accuracy of our predicitions vs labels
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+# ---- The Training Loop ----
+t = []
+# Store our loss and accuracy for plotting
+train_loss_set = []
+# Number of training epochs (authors recommend between 2 and 4)
+epochs = 4
+# trange is a tqdm wrapper around the normal python range
+for _ in trange(epochs, desc="Epoch"):
+  
+  
+  # Training
+  
+  # Set our model to training mode (as opposed to evaluation mode)
+  model.train()
+  
+  # Tracking variables
+  tr_loss = 0
+  nb_tr_examples, nb_tr_steps = 0, 0
+  
+  # Train the data for one epoch
+  for step, batch in enumerate(train_dataloader):
+    # Add batch to GPU
+    batch = tuple(t.to(device) for t in batch)
+    # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels = batch
+    # Clear out the gradients (by default they accumulate)
+    optimizer.zero_grad()
+    # Forward pass
+    outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
+    loss = outputs['loss']
+    train_loss_set.append(loss.item())    
+    # Backward pass
+    loss.backward()
+    # Update parameters and take a step using the computed gradient
+    optimizer.step()
+
+    # Update the learning rate.
+    scheduler.step()
+    
+    
+    # Update tracking variables
+    tr_loss += loss.item()
+    nb_tr_examples += b_input_ids.size(0)
+    nb_tr_steps += 1
+
+  print("Train loss: {}".format(tr_loss/nb_tr_steps))
+    
+    
+  # Validation
+
+  # Put model in evaluation mode to evaluate loss on the validation set
+  model.eval()
+
+  # Tracking variables 
+  eval_loss, eval_accuracy = 0, 0
+  nb_eval_steps, nb_eval_examples = 0, 0
+
+  # Evaluate data for one epoch
+  for batch in validation_dataloader:
+    # Add batch to GPU
+    batch = tuple(t.to(device) for t in batch)
+    # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels = batch
+    # Telling the model not to compute or store gradients, saving memory and speeding up validation
+    with torch.no_grad():
+      # Forward pass, calculate logit predictions
+      logits = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+    
+    # Move logits and labels to CPU
+    logits = logits['logits'].detach().cpu().numpy()
+    label_ids = b_labels.to('cpu').numpy()
+
+    tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+    
+    eval_accuracy += tmp_eval_accuracy
+    nb_eval_steps += 1
+
+  print("Validation Accuracy: {}".format(eval_accuracy/nb_eval_steps))
+
+# ---- Training Evaluation ----
+plt.figure(figsize=(15,8))
+plt.title("Training loss")
+plt.xlabel("Batch")
+plt.ylabel("Loss")
+plt.plot(train_loss_set)
+plt.show()
